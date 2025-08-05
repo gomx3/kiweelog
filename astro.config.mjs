77546d31594bf93f775a1678a78783653a56c198
Sowon Kim <sso626262@naver.com>
2025-08-06 00:46:26 +0900

    chore: update site URL in config file

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import mdx from '@astrojs/mdx'
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://kiweelog.netlify.app/',
+    site: 'https://kiweelog.vercel.app/',
     vite: {
         plugins: [tailwindcss()],
     },
