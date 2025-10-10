ed6096a2e395b662e4723c5f675b6121e122be11
Sowon Kim <sso626262@naver.com>
2025-10-11 01:40:03 +0900

    feat: apply meta tag for my blog

--- src/constants.ts
+++ src/constants.ts
@@ -1,5 +1,5 @@
 export const WEBPAGE_INFO = {
-    TITLE: 'Astro Learner | Blog',
-    DESCRIPTION: 'My journey learning Astro',
+    TITLE: 'Kiweelog | Blog',
+    DESCRIPTION: '개발자 김소원의 프로젝트, 기술 학습, 트러블슈팅 여정을 공유하는 블로그',
     LANGUAGE: 'ko-KR',
 } as const
