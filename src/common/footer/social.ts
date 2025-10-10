4739f07d596f046fdb3dcb44482448841ddc1437
Sowon Kim <sso626262@naver.com>
2025-10-11 01:29:46 +0900

    feat: modify blog main page UI

---
+++ src/common/footer/social.ts
@@ -0,0 +1,12 @@
+import githubIcon from '../../assets/icons/github.svg?raw'
+import linkedinIcon from '../../assets/icons/linkedin.svg?raw'
+
+export const SOCIAL_ICONS: Record<string, string> = {
+    github: githubIcon,
+    linkedin: linkedinIcon,
+}
+
+export const socialLinks: Record<string, string> = {
+    github: `https://github.com/gomx3`,
+    linkedin: `https://www.linkedin.com/in/%EC%86%8C%EC%9B%90-%EA%B9%80-b52314377/`,
+}
