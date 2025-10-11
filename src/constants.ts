aba4f251c82fc2a9c6aa774363c3a4afa4ffb9fa
Sowon Kim <sso626262@naver.com>
2025-10-12 03:01:42 +0900

    chore: add meta tags for SEO

--- src/constants.ts
+++ src/constants.ts
@@ -2,6 +2,8 @@ export const WEBPAGE_INFO = {
     TITLE: 'kiweelog | 김소원 기술 블로그',
     DESCRIPTION: '개발자 김소원의 프로젝트, 기술 학습, 트러블슈팅 여정을 공유하는 블로그',
     LANGUAGE: 'ko-KR',
+    IMAGE: '/logo/og.png',
+    URL: import.meta.env.RROD_URL,
 } as const
 
 export const GISCUS = {
