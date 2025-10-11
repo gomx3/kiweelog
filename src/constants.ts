5ef381bb48406891e9d615f47f0451dde4829e92
Sowon Kim <sso626262@naver.com>
2025-10-12 01:40:40 +0900

    feat: modify blog and tags page UI

--- src/constants.ts
+++ src/constants.ts
@@ -1,5 +1,5 @@
 export const WEBPAGE_INFO = {
-    TITLE: 'Kiweelog | Blog',
+    TITLE: 'kiweelog | 김소원 기술 블로그',
     DESCRIPTION: '개발자 김소원의 프로젝트, 기술 학습, 트러블슈팅 여정을 공유하는 블로그',
     LANGUAGE: 'ko-KR',
 } as const
