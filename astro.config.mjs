65a04d18be382df0ec6d249fa647a6261fe194ac
Sowon Kim <sso626262@naver.com>
2025-10-12 03:02:07 +0900

    chore: add @astrojs/sitemap settings

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,12 +4,13 @@ import { defineConfig } from 'astro/config'
 import tailwindcss from '@tailwindcss/vite'
 import react from '@astrojs/react'
 import mdx from '@astrojs/mdx'
+import sitemap from '@astrojs/sitemap'
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://kiweelog.vercel.app/',
+    site: 'https://kiweelog.netlify.app/',
     vite: {
         plugins: [tailwindcss()],
     },
-    integrations: [react(), mdx()],
+    integrations: [react(), mdx(), sitemap()],
 })
